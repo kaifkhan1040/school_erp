3b27051a9ff7ab831171d63814f65b34daa28b45
mohd kaif <kaifkhan1040@gmail.com>
2025-11-30 15:13:36 +0530

    message

--- erp/settings.py
+++ erp/settings.py
@@ -128,12 +128,21 @@ USE_I18N = True
 USE_TZ = True
 
 EMAIL_USE_TLS = True
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST_PASSWORD = 'Littlestar@2025'
+# EMAIL_HOST = 'smtp.hostinger.com'
+# EMAIL_HOST_USER = 'info@lspsgurugram.in'
+# DEFAULT_FROM_EMAIL = "info@lspsgurugram.in"
+# EMAIL_PORT = 587
+
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST_PASSWORD = 'Littlestar@2025'
-EMAIL_HOST = 'smtp.hostinger.com'
-EMAIL_HOST_USER = 'info@lspsgurugram.in'
-DEFAULT_FROM_EMAIL = "info@lspsgurugram.in"
+EMAIL_HOST = "smtp.pythonanywhere.com"
 EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "lspschoolerp"
+EMAIL_HOST_PASSWORD = "Root@123"
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
--- users/models.py
+++ users/models.py
@@ -60,17 +60,17 @@ class CustomUser(AbstractUser):
         self.image.storage.delete(self.image.name)
         super().delete()
         
-    def save(self, *args, **kwargs):
-        """ Delete old image when a new one is uploaded """
-        try:
-            old_user = CustomUser.objects.get(id=self.id)
-            if old_user.image and self.image and old_user.image != self.image:
-                if os.path.isfile(old_user.image.path):
-                    os.remove(old_user.image.path)
-        except CustomUser.DoesNotExist:
-            pass  
+    # def save(self, *args, **kwargs):
+    #     """ Delete old image when a new one is uploaded """
+    #     try:
+    #         old_user = CustomUser.objects.get(id=self.id)
+    #         if old_user.image and self.image and old_user.image != self.image:
+    #             if os.path.isfile(old_user.image.path):
+    #                 os.remove(old_user.image.path)
+    #     except CustomUser.DoesNotExist:
+    #         pass  
 
-        super().save(*args, **kwargs)
+    #     super().save(*args, **kwargs)
 
 
 class ForgetPassMailVerify(models.Model):
